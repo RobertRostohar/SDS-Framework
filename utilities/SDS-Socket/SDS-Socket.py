581a425b514484304397d7f3646ab0460d0296bf
DavidLesnjak <david.lesnjak@arm.com>
2022-12-22 10:19:06 +0100

    Update python script SDS-Socket (use TCP)

--- utilities/SDS-Socket/SDS-Socket.py
+++ utilities/SDS-Socket/SDS-Socket.py
@@ -14,140 +14,196 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from encodings import utf_8
-from os import path
+# SDS I/O TCP
+
+import sys
+
+import os.path as path
 import socket
-import atexit
 
-sock = socket.socket()
+class sdsio_manager:
+  def __init__(self):
+    self.stream_identifier = 0
+    self.stream_files = {}
 
-stream_identifier = 0
-stream_files = {}
+  # Open
+  def __open(self, mode, name):
+    file_index = 0
+    response = bytearray()
 
-# Command open
-def command_open(name, RdWr):
-  global stream_identifier
-  file_index = 0
+    if mode == 1:
+      # Write mode 
+      fname = f"{name}.{file_index}.sds"
+      while path.exists(fname) == True:
+        file_index = file_index + 1
+        fname = f"{name}.{file_index}.sds"
+      try:
+        f = open(fname, "wb")
+        self.stream_identifier += 1
+        self.stream_files.update({self.stream_identifier: f})
 
-  fname = f"{name}.{file_index}.sds"
-  while path.exists(fname) == True:
-    file_index = file_index + 1
-    fname = f"{name}.{file_index}.sds"
+        command   = 1
+        data_size = 0
+        response.extend(command.to_bytes(4, byteorder='little'))
+        response.extend(self.stream_identifier.to_bytes(4, byteorder='little'))
+        response.extend(mode.to_bytes(4, byteorder='little'))
+        response.extend(data_size.to_bytes(4, byteorder='little'))
+      except Exception as e:
+        print(f"Could not open file {fname}. Error: {e}\n")
+        return 0
 
-  try:
-    f = open(fname, "wb")
-    stream_identifier += 1
-    stream_files.update({stream_identifier: f})
-    return stream_identifier
-  except:
-    print(f"Can not open {fname}")
-    return 0
+      # if mode == 0:
+      #   read mode not supported
 
-# Command Close
-def command_close(id):
-  try:
-    stream_files.get(id).close()
-    stream_files.pop(id)
-    return 0x00000000
-  except:
-    print(f"Can not close {stream_files.get(id)}")
-    return 0xFFFFFFFF
+      return response
 
-# Command Write
-def command_write(id, data):
-  try:
-    stream_files.get(id).write(data)
-    return len(data)
-  except Exception as err:
-    print(f"Can not write to {stream_files.get(id)}")
-    print(f"  Error: {err}")
-    return 0  
 
-def set_communication ():
+  # Close
+  def __close(self, id):
+    response = bytearray()
+
+    try:
+      self.stream_files.get(id).close()
+      self.stream_files.pop(id)
+    except Exception as e:
+      print(f"Could not close file {self.stream_files.get(id)}. Error: {e}\n")
+    return response
+
+
+  # Write
+  def __write(self, id, data):
+    response = bytearray()
+
+    try:
+      self.stream_files.get(id).write(data)
+    except Exception as e:
+      print(f"Could not write to file {self.stream_files.get(id)}. Error: {e}\n")
+    return response
+
+
+  # Execute request
+  def execute_request (self, request_buf):
+    response = bytearray()
+
+    command    = int.from_bytes(request_buf[0:4],   'little')
+    sdsio_id   = int.from_bytes(request_buf[4:8],   'little')
+    argument   = int.from_bytes(request_buf[8:12],  'little')
+    data_size  = int.from_bytes(request_buf[12:16], 'little')
+    data       = request_buf[16:16 + data_size]
+
+    # Open
+    if command == 1:
+      response = self.__open(argument, data.decode('utf-8').split("\0")[0])
+    # Close 
+    elif command == 2:
+      self.__close(sdsio_id) 
+    # Write
+    elif command == 3:
+      self.__write(sdsio_id, data)
+    # Invalid command
+    else:
+      print(f"Invalid command: {command}")
+    return response
+
+
+# Start TCP Server
+def start_server ():
   #Get local IP
-  udp_ip = socket.gethostbyname(socket.gethostname())
-  print(f"{udp_ip}")
-  udp_port = 5000
-  #Create UDP socket (port 5000)
-  sock = socket.socket(socket.AF_INET,    # Internet
-                       socket.SOCK_DGRAM) # UDP
-  sock.bind((udp_ip, udp_port))
-  #sock.settimeout(5)
-  return sock
+  ip = socket.gethostbyname(socket.gethostname())
+  print(f"Start TCP Server.\n")
+  print(f"  Server IP: {ip}\n")
+  port = 5000
+  # Create TCP socket (port 5000)
+  sock_listening = socket.socket(socket.AF_INET,     # Internet
+                                 socket.SOCK_STREAM) # TCP
+  sock_listening.bind((ip, port))
+  sock_listening.listen()
+  return sock_listening
 
 # main
 def main():
-  response = bytearray()
-  request = bytearray()
-  packet_idx = 0
-  ret = 0
+  stream_buf_cnt    = 0
 
-  sock = set_communication()
+  header_size       = 16
+  header_buf        = bytearray()
 
-  while 1:
-    #try:
-    request, addr = sock.recvfrom(1500)
-    idx        = int.from_bytes(request[0:4],  'little')
-    command    = int.from_bytes(request[4:8],  'little')
-    argument   = int.from_bytes(request[8:12], 'little')
-    data_size  = int.from_bytes(request[12:16],'little')
-    data       = request[16:16+data_size]
+  request_buf_size  = 0
+  request_buf       = bytearray()
+ 
+  manager = sdsio_manager()
 
-    if idx == 0:
-      packet_idx = 0
-    
-    print(f"Command   {packet_idx}, {idx}")
-    if packet_idx == idx:
+  sock_listening = start_server()
+  sock, addr = sock_listening.accept()
+  print(f"Device connected.\n")
+  print(f"  Device IP: {addr}\n")
 
-      print(f"   {packet_idx} {command} {argument} {data_size} {data}")
-
-      # Execute command
-      # Open
-      if command == 1:
-        ret = command_open(data.decode('utf-8').split("\0")[0], argument)
-      # Close 
-      elif command == 2:
-        ret = command_close(argument) 
-      # Write
-      elif command == 3:
-        ret = command_write(argument, data)
-
-      # Prepare response
-      response_size = 4
-      response[0:4]   = packet_idx.to_bytes(4, byteorder='little')
-      response[4:8]   = command.to_bytes(4, byteorder='little')
-      response[8:12]  = argument.to_bytes(4, byteorder='little')
-      response[12:16] = response_size.to_bytes(4, byteorder='little')
-      response[16:20] = ret.to_bytes(4, byteorder='little')
-
-    # Send Response
-    print("Response")
-    print(f"   {packet_idx} {command} {argument} {response} {ret}")
+  while True:
     try:
-      resp = bytes(response)
-      sock.sendto(bytes(resp), addr)
-      if packet_idx == idx:
-        packet_idx = packet_idx + 1
-      print("        Response successfully sent!")
-    except:        
-      print("        Unable to send response")
+      # Read new stream data
+      stream_buf = sock.recv(8192)
+      if not stream_buf:
+        # Close socket
+        sock.close()
+        print("Device disconnected.\n")
 
-    #except socket.timeout:
-    #  continue
+        # Wait device to connect
+        sock, addr = sock_listening.accept()
+        print(f"Device connected.\n")
+        print(f"  Device IP: {addr}\n")
+    except Exception as e: 
+      print(f"Socket recv error: {e}\n")
+      sys.exit(1)
 
+    stream_buf_cnt = 0
 
-def exit():
-  # Close all .sds files
-  for file in stream_files.values():
-    try:
-      file.close()
-    except:        
-      print("Can not close .sds file")
-  #Close Socket
-  sock.close()
+    while stream_buf_cnt < len(stream_buf):
+      
+      if request_buf_size == 0:
+        # Request buffer is empty. Get new request
+        cnt = header_size - len(header_buf)
+        if cnt > (len(stream_buf) - stream_buf_cnt):
+          cnt = len(stream_buf) - stream_buf_cnt
+        header_buf.extend(stream_buf[stream_buf_cnt: stream_buf_cnt + cnt])
+        stream_buf_cnt += cnt
 
+        if len(header_buf) != header_size:
+          # Header not complete. Read new data
+          break
+        else:
+          # New request
+          request_buf = bytearray()
+          request_buf.extend(header_buf)
+          request_buf_size = int.from_bytes(header_buf[12: 16], 'little') + header_size
+          # Clear Header buffer
+          del header_buf[0:]
 
+      cnt = request_buf_size - len(request_buf)
+      if cnt > (len(stream_buf) - stream_buf_cnt):
+        # Not all data is yet available.
+        cnt = len(stream_buf) - stream_buf_cnt
+
+      # Copy request data
+      request_buf.extend(stream_buf[stream_buf_cnt : stream_buf_cnt + cnt])
+
+      # Update data count
+      stream_buf_cnt += cnt
+
+      if len(request_buf) == request_buf_size:
+        # Whole request is prepared. Execute request.
+        response = manager.execute_request(request_buf)
+
+        # Reset request buffer size. New request can now be processed.
+        request_buf_size = 0
+
+        # Send response
+        if response:
+          try:
+            sock.send(bytes(response))
+          except socket.error as e: 
+            print(f"Socket send error: {e}\n")
+            sys.exit(1)
+
+# main
 if __name__ == '__main__':
-    atexit.register(exit)
-    main()
+  main()
 
